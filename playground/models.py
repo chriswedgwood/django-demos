cdd97c6ccc714e9086320b22cef87ba179f59d6c
Chris Wedgwood <wedgemail@gmail.com>
2022-01-10 07:59:28 +0200

    questions initial

--- playground/models.py
+++ playground/models.py
@@ -1,10 +1,10 @@
 from django.db import models
+from django.utils.translation import gettext as _
 
 
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
-    pub_date = models.DateTimeField('date published')
-
+    pub_date = models.DateTimeField("date published")
 
     def was_published_recently(self):
         return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
@@ -21,9 +21,9 @@ class Person(models.Model):
     last_name = models.CharField(max_length=30)
     birth_date = models.DateField()
     SHIRT_SIZES = (
-        ('S', 'Small'),
-        ('M', 'Medium'),
-        ('L', 'Large'),
+        ("S", "Small"),
+        ("M", "Medium"),
+        ("L", "Large"),
     )
     name = models.CharField(max_length=60)
     shirt_size = models.CharField(max_length=1, choices=SHIRT_SIZES)
@@ -31,6 +31,7 @@ class Person(models.Model):
     def baby_boomer_status(self):
         "Returns the person's baby-boomer status."
         import datetime
+
         if self.birth_date < datetime.date(1945, 8, 1):
             return "Pre-boomer"
         elif self.birth_date < datetime.date(1965, 1, 1):
@@ -38,17 +39,17 @@ class Person(models.Model):
         else:
             return "Post-boomer"
 
-
     def __str__(self):
         return self.first_name
 
 
 class Group(models.Model):
     name = models.CharField(max_length=128)
-    members = models.ManyToManyField(Person, through='Membership')
+    members = models.ManyToManyField(Person, through="Membership")
 
     def __str__(self):
         return self.name
+
 
 class Membership(models.Model):
     person = models.ForeignKey(Person, on_delete=models.CASCADE)
@@ -58,21 +59,20 @@ class Membership(models.Model):
 
 
 class Runner(models.Model):
-    MedalType = models.TextChoices('MedalType', 'GOLD SILVER BRONZE')
+    MedalType = models.TextChoices("MedalType", "GOLD SILVER BRONZE")
     name = models.CharField(max_length=60)
     medal = models.CharField(blank=True, choices=MedalType.choices, max_length=10)
-
 
 
 class Fruit(models.Model):
     name = models.CharField(max_length=100, primary_key=True)
 
 
-
 class Musician(models.Model):
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
     instrument = models.CharField(max_length=100)
+
 
 class Album(models.Model):
     artist = models.ForeignKey(Musician, on_delete=models.CASCADE)
@@ -81,24 +81,21 @@ class Album(models.Model):
     num_stars = models.IntegerField()
 
 
-
-
 class Manufacturer(models.Model):
     # ...
     pass
+
 
 class Car(models.Model):
     manufacturer = models.ForeignKey(Manufacturer, on_delete=models.CASCADE)
 
 
-
-
 class Topping(models.Model):
-    # ...
-    pass
+    title = models.CharField(_(""), max_length=100, null=False)
+
 
 class Pizza(models.Model):
-    # ...
+    title = models.CharField(_(""), max_length=100, null=False)
     toppings = models.ManyToManyField(Topping)
 
 
@@ -115,10 +112,10 @@ class Restaurant(models.Model):
         null=True,
     )
 
+
 class Ox(models.Model):
     horn_length = models.IntegerField()
 
     class Meta:
         ordering = ["horn_length"]
         verbose_name_plural = "oxen"
-        
