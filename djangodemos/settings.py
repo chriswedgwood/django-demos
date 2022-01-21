47f30139e574bebdab962e27d4da2ce434eb598a
Chris Wedgwood <wedgemail@gmail.com>
2022-01-21 11:32:59 +0000

    STATIC_ROOT

--- djangodemos/settings.py
+++ djangodemos/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import environ
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -136,6 +137,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = "/static/"
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
 STATICFILES_DIRS = [str(APPS_DIR / "static")]
 
